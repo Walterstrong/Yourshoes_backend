df1527484f75e1c31990d3e9f9ed70904e34b23e
walter <tojimatovabdukarim@gmail.com>
2022-09-21 00:50:59 +0900

    member controlled created

---
+++ controllers/memberController.js
@@ -0,0 +1,21 @@
+let memberController=module.exports;
+
+memberController.home=(req,res)=>{
+    console.log("GET cont.home");
+    res.send("you are at home");
+};
+
+memberController.signup=(req,res)=>{
+    console.log("POS cont.home");
+    res.send("you are at signup");
+};
+
+memberController.login=(req,res)=>{
+    console.log("POS cont.home");
+    res.send("you are at login");
+};
+
+memberController.logout=(req,res)=>{
+    console.log("GET cont.home");
+    res.send("you are at logout");
+};
