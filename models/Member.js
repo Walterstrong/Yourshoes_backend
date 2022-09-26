98e9134c98fcc1d44fdb68ff5b873104d4fc42d1
walter <tojimatovabdukarim@gmail.com>
2022-09-27 00:38:02 +0900

    creating bcrypt hash and compare

--- models/Member.js
+++ models/Member.js
@@ -1,6 +1,7 @@
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/ mistake");
 const assert = require("assert");
+const bcrypt = require("bcryptjs");
 
 class Member {
   constructor() {
@@ -9,6 +10,8 @@ class Member {
 
   async signupData(input) {
     try {
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
       const new_member = new this.memberModel(input);
       let result;
       try {
@@ -32,7 +35,10 @@ class Member {
         .exec();
       assert.ok(member, Definer.auth_err3);
 
-      const isMatch = input.mb_password === member.mb_password;
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
       assert.ok(isMatch, Definer.auth_err4);
 
       return await this.memberModel
