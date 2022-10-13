f6e02038e38d0c6dfddf9c6942b7268f38d281e7
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-13 20:38:39 +0900

    task10 was fixed

--- task_10.js
+++ task_10.js
@@ -1,12 +1,15 @@
 //Bu yerda berilgan string qiymat function ichiga pass qilinayabdi. Function shartiga esa, string nechta harf hamda sonlardan tashkil topganligini object sifatida qaytarish logicasi kiritilgan.
 
-let string = "ks2ldjfkldsj3431143";
+let string = "1133As";
+
 function test(string) {
   const raqamlarSoni = string.replace(/[^0-9]/g, "").length;
-  const harflarSoni = string.replace(/[^a-z]/g, "").length;
+  const harflarSoni = string.replace(/[^a-zA-Z]/g, "").length;
   const natija = { digits: raqamlarSoni, letters: harflarSoni };
+
   return natija;
 }
 
 const result = test(string);
+
 console.log("javob:", result);
