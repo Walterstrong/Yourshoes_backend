709895c7c55dd709dbddfb37f78b7f98ffe371b8
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-16 18:32:00 +0900

    fixed

--- task_10.js
+++ task_10.js
@@ -1,15 +1,24 @@
-//Bu yerda berilgan string qiymat function ichiga pass qilinayabdi. Function shartiga esa, string nechta harf hamda sonlardan tashkil topganligini object sifatida qaytarish logicasi kiritilgan.
+// //Bu yerda berilgan string qiymat function ichiga pass qilinayabdi. Function shartiga esa, string nechta harf hamda sonlardan tashkil topganligini object sifatida qaytarish logicasi kiritilgan.
 
-let string = "1133As";
+// let string = "1133As";
 
-function test(string) {
-  const raqamlarSoni = string.replace(/[^0-9]/g, "").length;
-  const harflarSoni = string.replace(/[^a-zA-Z]/g, "").length;
-  const natija = { digits: raqamlarSoni, letters: harflarSoni };
+// function test(string) {
+//   const raqamlarSoni = string.replace(/[^0-9]/g, "").length;
+//   const harflarSoni = string.replace(/[^a-zA-Z]/g, "").length;
+//   const natija = { digits: raqamlarSoni, letters: harflarSoni };
 
-  return natija;
-}
+//   return natija;
+// }
 
-const result = test(string);
+// const result = test(string);
 
-console.log("javob:", result);
+// console.log("javob:", result);
+
+const findIndex = (arr, n, K) => {};
+
+let arr = [1, 2, 3, 4, 5];
+let n = arr.length;
+let K = 7;
+
+const result = findIndex(arr, n, K);
+console.log("result: ", result);
