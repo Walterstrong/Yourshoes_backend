85338f00dc8cee8d1eaf1dc6d2b7afcc20d77d39
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-16 23:33:34 +0900

    fix: modigy logout business logic

--- controllers/restaurantController .js
+++ controllers/restaurantController .js
@@ -86,10 +86,18 @@ restaurantController.loginProcess = async (req, res) => {
   }
 };
 
-restaurantController.logout = (req, res) => {
-  console.log("GET cont.home");
-  res.send("you are at logout");
+restaurantController.logout = async (req, res) => {
+  try {
+    console.log("GET cont/logout");
+    req.session.destroy(function () {
+      res.redirect("/resto");
+    });
+  } catch (err) {
+    console.log(`ERROR, cont/logout, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
+
 restaurantController.validateAuthRestaurant = (req, res, next) => {
   if (req.session?.member?.mb_type === "RESTAURANT") {
     req.member = req.session.member;
