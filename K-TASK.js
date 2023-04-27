2f69d061633467ba8c8271404d23bbe5c5de8611
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2023-04-27 19:50:40 +0900

    feat: new task changed

--- K-TASK.js
+++ K-TASK.js
@@ -1,8 +1,28 @@
-let sonlar = [1, 2, 3, 4, 10, 5, 6, 7, 8];
-function test(sonlar) {
-  const a = Math.max(...sonlar);
-  return a;
+// let sonlar = [1, 2, 3, 4, 10, 5, 6, 7, 8];
+// function test(sonlar) {
+//   const a = Math.max(...sonlar);
+//   return a;
+// }
+
+// const result = test(sonlar);
+// console.log(result);
+
+function countLetterOccurrences(letter, word) {
+  let count = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
-const result = test(sonlar);
-console.log(result);
+// Example usage:
+let letter = "a";
+let word = "banana";
+let occurrences = countLetterOccurrences(letter, word);
+console.log(
+  `The letter '${letter}' occurs ${occurrences} times in the word '${word}'.`
+);
