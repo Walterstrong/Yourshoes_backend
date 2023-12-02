902c633a367b881525fb43c4c702943f1a84a0ed
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2023-12-02 16:35:39 +0500

    fix: changed docker container running eniveronment

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ async function connectDB() {
 
     // Start the server after successful connection
     const server = require("./app.js");
-    const host = "0.0.0.0"; // Add this line
-    const port = process.env.PORT || 3003; // Assuming your port is 3003
+    //const host = "0.0.0.0"; // Add this line
+    const PORT = process.env.PORT || 3003; // Assuming your port is 3003
 
-    server.listen(port, host, () => {
-      console.log(`Server running at http://${host}:${port}/`);
+    server.listen(PORT, () => {
+      console.log(`Server running at http://${PORT}/`);
     });
   } catch (err) {
     console.error("ERROR on connection MongoDB:", err.message);
